36af87d7bceaf738f155d89806a0aedbf37e60dc
JulioTrujilloA <julio.trujillo.alvarado@outlook.com>
2018-06-12 12:13:48 -0500

    Tercera versión del programa

--- src/com/holamundo/main/HolaMundo.java
+++ src/com/holamundo/main/HolaMundo.java
@@ -5,6 +5,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("1. Otro ejemplo de proyecto");
 		System.out.println("2. Se añade ésta línea para comprobar la subida de GitHub");
+		System.out.println("3. Para la tercera versión");
 	}
 
 }
